355f8a34ef46d1190543145e0a02e276ca7169ae
jcbegs_CCO <john-carl.begley@charite.de>
2024-11-18 13:16:06 +0100

    Rerun setup.py; move .bat files to Scripts

--- src/gui_config/Scripts/run_s2p_gui.bat
+++ src/gui_config/Scripts/run_s2p_gui.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Activate the s2p environment
-CALL "C:\Users\Justus\Anaconda3\Scripts\activate.bat" suite2p
+CALL "C:\miniforge3\Scripts\activate.bat" suite2p
 
 python -m suite2p
 
--- src/gui_config/Scripts/run_sequence.bat
+++ src/gui_config/Scripts/run_sequence.bat
@@ -2,23 +2,14 @@
 
 :: Activate the first virtual environment, evt. use Activate.ps1
 
-CALL "C:\Users\Justus\Anaconda3\Scripts\activate.bat" suite2p
+CALL "C:\miniforge3\envs\suite2p\Scripts\activate.bat" suite2p
 
 :: Run the first script
-python -m suite2p_detect
+python -m syn_process
 
 :: Deactivate the first environment
 CALL conda deactivate
-
-:: Activate the second virtual environment
-CALL "C:\Users\Justus\Anaconda3\Scripts\activate.bat" cascade
-
-:: Run the second script
-python -m cascade_deconvolve
-
-:: Deactivate the second environment
-CALL conda deactivate
-
+pause
 :: Activate the third virtual environment
 CALL "C:\Users\Justus\Anaconda3\Scripts\activate.bat" data_env
 
