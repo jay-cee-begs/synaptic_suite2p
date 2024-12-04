2ef0a9831e0badfcbac8bbe3438debdb864d995a
jcbegs <jcbegs@gmail.com>
2024-12-04 12:08:10 +0100

    Add batch file to launch GUI from desktop

---
+++ src/gui_config/Scripts/gui_launcher.bat
@@ -0,0 +1,16 @@
+@echo off
+
+:: Activate the s2p environment
+CALL "C:\Users\jcbeg\pyenv\suite2p\Scripts\activate.bat" 
+
+:: Run the first script
+
+set script_dir=%~dp0
+
+@REM set src_dir=%script_dir%..\..\
+
+
+cd "%script_dir%..\"
+
+
+python -m synapse_gui
