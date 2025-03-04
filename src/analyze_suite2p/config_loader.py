bbcecb8f8483d42de3171fe26e0f5971cc9ea571
jay-cee-begs <john-carl.begley@charite.de>
2025-03-04 19:29:52 +0100

    update config_loader to take config file path

--- src/analyze_suite2p/config_loader.py
+++ src/analyze_suite2p/config_loader.py
@@ -3,11 +3,15 @@ from pathlib import Path
 from types import SimpleNamespace
 
 
-def load_json_config_file():
-    script_dir = Path(__file__).resolve().parent  # Get current script directory (project/src/gui_config)
-    json_filepath = (script_dir / "../../config/config.json").resolve()
-    with open(json_filepath, 'r') as f:
-        config_dict = json.load(f)
+def load_json_config_file(config_path = None):
+    if config_path is not None: 
+        with open(config_path, 'r') as f:
+            config_dict = json.load(f)       
+    else:
+        script_dir = Path(__file__).resolve().parent  # Get current script directory (project/src/gui_config)
+        json_filepath = (script_dir / "../../config/config.json").resolve()
+        with open(json_filepath, 'r') as f:
+            config_dict = json.load(f)
     return json.loads(json.dumps(config_dict), object_hook=lambda d: SimpleNamespace(**d))
 
 config = load_json_config_file()
