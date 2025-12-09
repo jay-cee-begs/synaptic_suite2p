16f891dafc667255d5a7f821d9f5b4a7bb93e45e
jay-cee-begs <john-carl.begley@charite.de>
2025-12-09 17:35:26 +0100

    Add Docstrings to config_loader.py functions
    * Add DocStrings to functions loading JSON configurations files either as a SimpleNamespace dictionary object or as a raw dictionary
    
    * SimpleNamespace objects are easier to index quickly compared to dictionaries (in my opinion)

--- src/analyze_suite2p/config_loader.py
+++ src/analyze_suite2p/config_loader.py
@@ -4,6 +4,23 @@ from types import SimpleNamespace
 
 
 def load_json_config_file(config_path = None):
+    """
+    Load config.json synapse_gui configurations file as SimpleNamespace object
+
+    Args:
+    -----
+    config_path: str, optional
+        Path to JSON configurations file (config.json)
+    
+    Returns:
+    --------
+    SimpleNamespace JSON dictionary
+
+    Example:
+        >>> load_json_config_file()
+        {config.general_settings.main_folder, config.general_settings.groups, 
+        config.analysis_params.overwrite_suite2p, ...} 
+    """
     if config_path is not None: 
         with open(config_path, 'r') as f:
             config_dict = json.load(f)       
@@ -17,6 +34,23 @@ def load_json_config_file(config_path = None):
 config = load_json_config_file()
 
 def load_json_dict():
+    """
+    Load config.json synapse_gui configurations file as a dictionary
+
+    Args:
+    -----
+    config_path: str, optional
+        Path to JSON configurations file (config.json)
+    
+    Returns:
+    --------
+    config_dict: dictionary
+
+    Example:
+        >>> load_json_config_file()
+        {config['general_settings']['main_folder'], config['general_settings']['groups'], 
+        config['analysis_params']['overwrite_suite2p'], ...} 
+    """
     script_dir = Path(__file__).resolve().parent  # Get current script directory (project/src/gui_config)
     json_filepath = (script_dir / "../../config/config.json").resolve()
     with open(json_filepath, 'r') as f:
