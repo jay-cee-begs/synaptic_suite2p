f708c8b4e7379a88e106b6430774de244e3935f4
jay-cee-begs <john-carl.begley@charite.de>
2026-03-27 17:38:55 +0100

    Clean plotting_utility.py to remove legacy code

--- src/analyze_suite2p/plotting_utility.py
+++ src/analyze_suite2p/plotting_utility.py
@@ -3,84 +3,19 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib.colors import Normalize
 import matplotlib.cm as cm
-import seaborn as sns
 import scipy.stats as stats
-from scipy.stats import norm
 from scipy.signal import find_peaks
 from scipy.ndimage import binary_dilation, binary_fill_holes
 from PIL import Image
 from BaselineRemoval import BaselineRemoval
-import pickle
-import pynapple as nap
+from pathlib import Path
 
 from analyze_suite2p import detector_utility, config_loader
 
 _DEFAULT_CONFIG = config_loader.load_json_config_file()
 config = _DEFAULT_CONFIG
 
-"""
-below is an example structure for a dictionary for all the experiment files 
-suite2one might implement this code as the following in a processing_pipeline:
-    experiment_structure = {
-    "control": {
-        "replicate_1": [f"control_A_well_0{j}Dur180sInt100msBin500ms_filtered.pkl" for j in range(1, 5)],
-        "replicate_2": [f"control_A_well_0{j}Dur180sInt100msBin500ms_filtered.pkl" for j in range(5, 9)],
-    },
-    "treatment": {
-        "replicate_1": [f"treatment_B_well_0{j}Dur180sInt100msBin500ms_filtered.pkl" for j in range(1, 5)],
-        "replicate_2": [f"treatment_B_well_0{j}Dur180sInt100msBin500ms_filtered.pkl" for j in range(5, 9)],
-    }
-}
-"""
-_experiment_structure_example = {
-    "control": {
-        "replicate01": ["file1", "file2"]
-    },
-    "treatment1": {
-        "replicate01": ["file3", "file4"]
-    },
-    "treatment2": {
-        "replicate01": ["file1", "file2"]
-    },
-    "treatment3": {
-        "replicate01": ["file3", "file4"]
-    },
-}
-
-
-_available_tests = {
-    "mann-whitney-u": stats.mannwhitneyu,
-    "wilcoxon": stats.wilcoxon,
-    "paired_t": stats.ttest_rel,
-}
-def get_significance_text(series1, series2, test="mann-whitney-u", bonferroni_correction=1, show_ns=False, 
-                          cutoff_dict={"*":0.05, "**":0.01, "***":0.001, "****":0.00099}, return_string="{text}\n{pvalue:.4f}"):
-    
-    """Function to calculate significance for data to plot
-    INPUTS:
-    series 1: first sample
-    series 2: second sample
-    test: chosen from scipy.stats"""
-
-    statistic, pvalue = _available_tests[test](series1, series2)
-    levels, cutoffs = np.vstack(list(cutoff_dict.items())).T
-    levels = np.insert(levels, 0, "n.s." if show_ns else "")
-    text = levels[(pvalue < cutoffs.astype(float)).sum()]
-    return return_string.format(pvalue=pvalue, text=text) #, text=text
-
-def add_significance_bar_to_axis(ax, series1, series2, center_x, line_width):
-    """Function to add significance stars to figures"""
-    significance_text = get_significance_text(series1, series2, show_ns=True)
-    
-    original_limits = ax.get_ylim()
-    
-    ax.errorbar(center_x, original_limits[1], xerr=line_width/2, color="k", capsize=12)
-    ax.text(center_x, original_limits[1], significance_text, ha="center", va="bottom", fontsize = 32)
-    
-    extended_limits = (original_limits[0], (original_limits[1] - original_limits[0]) * 1.2 + original_limits[0])
-    ax.set_ylim(extended_limits)
 def single_spine_peak_plotting(deltaF, threshold_multiplier):
     """
     Function to plot single synaptic trace with threshold and example peak detection
@@ -97,21 +32,29 @@ def single_spine_peak_plotting(deltaF, threshold_multiplier):
     ----------
         None  
     """
+    sigma, deltaF_baseline = detector_utility.estimate_single_trace_baseline_noise_mad(deltaF, event_threshold=2)
     
-    return ax
+    baseline_reference = np.median(deltaF_baseline)
+    peak_detection_multiplier = threshold_multiplier
+    threshold = np.median(deltaF_baseline) + (peak_detection_multiplier * sigma)
 
-def aggregated_feature_plot(experiment_df, feature="SpikesFreq", agg_function="median", comparison_function="mean",
-                            palette="Set3", significance_check=False, group_order=None, control_group = None, ylim = 0, y_label = "", x_label = ""):
+    peaks, _ = find_peaks(deltaF, height = threshold, distance = 5, prominence = baseline_reference + sigma, width = (2,None))
+    amplitudes = deltaF[peaks] - baseline_reference #amplitude
+    plt.figure(figsize=[10,10])
+    plt.plot(deltaF)
+    plt.plot(peaks, deltaF[peaks], "x")
+
+    plt.plot(np.full_like(deltaF, threshold), "--",color = "red", label = 'Threshold')
+    plt.plot(np.full_like(deltaF, np.median(deltaF_baseline)), "--", color = 'black')
+
+    print('the peak time stamps are :{}' .format(peaks))
+    print('the amplitude of each peak is: {}' .format(amplitudes))
+    plt.legend()
+    plt.show()
+
+
+def plot_raw_deltaF_vs_airPLS_correction(deltaF, lambda_values = [100,1000], ylim = (-0.2,0.9)):
     """
-    Plot summary statistics for aggregated calcium imaging dataframe
-    INPUTS:
-    experiment_df: parent dataframe
-    feature: column to aggregate in df
-    agg_function: mean or median (median less sensitive to outliers)
-    comparison_function: comparing between groups
-    signficance_check: list of lists of groups to compare
-    group_order: order of experimental conditions
-    control_group: for normalizing values to a control conditions
     Function to illustrate difference between normalized deltaF with and without baseline correction via airPLS
 
     Args:
@@ -127,96 +70,27 @@ def aggregated_feature_plot(experiment_df, feature="SpikesFreq", agg_function="m
     ----------
         None
     """
-    numeric_df = experiment_df.select_dtypes(include='number')
+    # Define lambda values to test
+    lambda_values = [1000, 100]
 
-    if feature not in numeric_df.columns:
-        raise ValueError(f"The specified feature '{feature}' is not numeric")
+    # Create a figure for visualization
+    plt.figure(figsize=(12, 6))
+    plt.plot(deltaF, label='Original dF/F Trace', color='black', linewidth=1, alpha = 0.3)
 
-    grouped_df = experiment_df.groupby(["Experimental_Group", "File Name"]).agg(agg_function).reset_index(drop=False) #"dataset",
+    # Apply baseline correction with different lambda values and plot
+    for lam in lambda_values:
+        corrected_signal = BaselineRemoval(deltaF)
+        trace = corrected_signal.ZhangFit(lambda_=lam, repitition=100)
+        plt.plot(trace, label=f'Corrected (lambda={lam})', alpha = 0.5)
 
-    if control_group is not None:
-        control_avg = grouped_df[grouped_df['Experimental_Group'] == control_group][feature].agg(comparison_function)
-
-        grouped_df[feature] = grouped_df[feature].apply(lambda x: (x / control_avg) * 100)
-    else:
-        grouped_df = grouped_df
-
-    fig = plt.figure(figsize=(48, 16))
-    ax = fig.add_subplot()
-    color_palette = sns.color_palette(palette)
-
-    def get_kde(data):
-        """
-        Custom function to calculate the quartiles and add dashed lines to the violin plots.
-        """
-        q25, q50, q75 = np.percentile(data, [25, 50, 75])
-        ax.axvline(q25, linestyle="--", color="black", alpha=1, ymin=0, ymax=1)
-        ax.axvline(q50, linestyle="--", color="black", alpha=1, ymin=0, ymax=1)
-        ax.axvline(q75, linestyle="--", color="black", alpha=1, ymin=0, ymax=1)
-        return sns.kdeplot(data, color="black", ax=ax, bw_adjust=0.5, clip=(0, 1))
-
-    # Use 'group_order' in the sns.violinplot call to control the order of groups on the x-axis.
-    sns.violinplot(x="Experimental_Group", y=feature, data=grouped_df, ax=ax, palette=palette, order=group_order,
-                    inner="quartile", width=0.5) #, scale = 'area' inner="quartile",   get_kde=get_kde, , fontsize=44
-    # Use 'group_order' in the sns.violinplot call to control the order of groups on the x-axis.
-    # sns.violinplot(x="group", y=feature, data=grouped_df, ax=ax, palette=palette, order=group_order) #hue="dataset"
-
-    # marker_width = 0.5
-    # Update tick_positions to match the provided 'group_order' if it's specified, ensuring the custom order is used.
-    if group_order:
-        tick_positions = {group: pos for pos, group in enumerate(group_order)}
-    else:
-        tick_positions = {ax.get_xticklabels()[index].get_text(): ax.get_xticks()[index] for index in
-                          range(len(ax.get_xticklabels()))}
-
-    # for group, group_df in grouped_df.groupby("group"):
-    #     for dataset_index, (dataset, dataset_df) in enumerate(group_df.groupby("dataset")):
-    #         feature_mean = dataset_df[feature].agg(comparison_function)
-    #         ax.plot([tick_positions[group] - marker_width/2, tick_positions[group] + marker_width/2],
-    #                 [feature_mean, feature_mean], "-", color=color_palette[dataset_index], lw=2)
-
-    if significance_check:
-        sub_checks = [significance_check] if not any(isinstance(element, list) for element in significance_check) else significance_check
-        for sub_check in sub_checks:
-            add_significance_bar_to_axis(ax, 
-                                 grouped_df[grouped_df["Experimental_Group"] == sub_check[0]][feature], 
-                                 grouped_df[grouped_df["Experimental_Group"] == sub_check[1]][feature],
-                                (tick_positions[sub_check[0]] + tick_positions[sub_check[1]]) / 2,
-                                abs(tick_positions[sub_check[0]] - tick_positions[sub_check[1]]))
-
-    ax.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0, frameon=False)
-
-    ax.set_ylim([ylim, ax.get_ylim()[1]])
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-    ax.set_ylabel(y_label, fontsize = 64)
-    ax.set_xlabel(x_label, fontsize = 64)
-    ax.set_xticklabels(ax.get_xticklabels(), fontsize=44)
-    ax.set_yticklabels(ax.get_yticklabels(), fontsize=44)
-
-    return fig
-
-def build_experiment_dfs(input_path, experiment_structure):
-    """Function to build experiment dataframe from pickle files; currently performs for binned stats and cell stats; only need cell stats in future"""
-    experiment_cell_stats, experiment_binned_stats = pd.DataFrame(), pd.DataFrame()
-
-    for group in experiment_structure.keys():
-        for dataset in experiment_structure[group].keys():
-            try:
-                for file_name in experiment_structure[group][dataset]:
-                    # try: TODO trouble shoot why file not found error pops up here
-                        file_dict = pd.read_pickle(os.path.join(input_path, file_name))
-                        cell_stats, binned_stats = file_dict["cell_stats"], file_dict["binned_stats"]
-                        
-                        for stats, experiment_stats in zip((cell_stats, binned_stats), 
-                                                        (experiment_cell_stats, experiment_binned_stats)):
-                            stats[["group", "dataset", "file_name"]] = group, dataset, file_name
-                        experiment_cell_stats = pd.concat((experiment_cell_stats, cell_stats))
-                        experiment_binned_stats = pd.concat((experiment_binned_stats, binned_stats))
-            except Exception as e:
-                        print(f"Error processing file {file_name}: {e}")
-                
-    return experiment_cell_stats, experiment_binned_stats
+    # Show plot
+    plt.legend()
+    plt.title('Effect of Lambda on Baseline Correction (ZhangFit)')
+    plt.xlabel('Time')
+    plt.ylabel('dF/F')
+    plt.ylim(ylim)
+    plt.grid()
+    plt.show()
 
 def get_all_pkl_outputs_in_path(path):
     """
@@ -781,71 +655,3 @@ def create_suite2p_ROI_masks(stat, frame_shape, nid2idx, output_path):
     im = Image.fromarray(roi_masks)
     im.save(output_path)
     return im, roi_masks
-
-def single_spine_plotting_new(deltaF):
-    """MUST HAVE airPLS FIT THEN dF/F0"""
-    sigma, deltaF_baseline = detector_utility.estimate_single_trace_baseline_noise_mad(deltaF, event_threshold=2)
-    
-    baseline_reference = np.median(deltaF_baseline)
-    peak_detection_multiplier = 4.5# float(config.analysis_params.peak_detection_threshold)
-    threshold = np.median(deltaF_baseline) + (peak_detection_multiplier * sigma)
-
-    peaks, _ = find_peaks(deltaF, height = threshold, distance = 5, prominence = baseline_reference + sigma, width = (2,None))
-
-def single_spine_peak_plotting(deltaF):
-    
-    sigma, deltaF_baseline = detector_utility.estimate_single_trace_baseline_noise_mad(deltaF, event_threshold=2)
-    
-    baseline_reference = np.median(deltaF_baseline)
-    peak_detection_multiplier = 4.5# float(config.analysis_params.peak_detection_threshold)
-    threshold = np.median(deltaF_baseline) + (peak_detection_multiplier * sigma)
-
-    peaks, _ = find_peaks(deltaF, height = threshold, distance = 5, prominence = baseline_reference + sigma, width = (2,None))
-
-    amplitudes = deltaF[peaks] - baseline_reference #amplitude
-
-    plt.figure(figsize=[10,10])
-    plt.plot(deltaF)
-    plt.plot(peaks, deltaF[peaks], "x")
-
-    plt.plot(np.full_like(deltaF, threshold), "--",color = "red")
-    plt.plot(np.full_like(deltaF, np.median(deltaF_baseline)), "--", color = 'black')
-
-    print('the peak time stamps are :{}' .format(peaks))
-    print('the amplitude of each peak is: {}' .format(amplitudes))
-    plt.legend()
-
-    plt.show()
-    print(f" DeltaF Trace Median: {np.median(deltaF)}")
-    print(f"DeltaF Trace Mode: {stats.mode(deltaF)}")
-    plt.figure(figsize=[10,7])
-    plt.hist(deltaF, bins = 1000)
-    plt.xlabel("dF/F Normalized Fluorescence", fontsize = 20)
-    plt.ylabel("Number of Frames per bin", fontsize = 20)
-    plt.legend()
-    plt.tight_layout
-    # plt.show()
-
-
-def plot_raw_deltaF_vs_airPLS_correction(deltaF, lambda_values = [100,1000], ylim = (-0.2,0.9)):
-    # Define lambda values to test
-    lambda_values = [1000, 100]
-
-    # Create a figure for visualization
-    plt.figure(figsize=(12, 6))
-    plt.plot(deltaF, label='Original dF/F Trace', color='black', linewidth=1, alpha = 0.3)
-
-    # Apply baseline correction with different lambda values and plot
-    for lam in lambda_values:
-        corrected_signal = BaselineRemoval(deltaF)
-        trace = corrected_signal.ZhangFit(lambda_=lam, repitition=100)
-        plt.plot(trace, label=f'Corrected (lambda={lam})', alpha = 0.5)
-
-    # Show plot
-    plt.legend()
-    plt.title('Effect of Lambda on Baseline Correction (ZhangFit)')
-    plt.xlabel('Time')
-    plt.ylabel('dF/F')
-    plt.ylim(ylim)
-    plt.grid()
-    plt.show()
