26a49bb40d33c4b61a910f0dd6ee422388254bc7
JC_home <jcbegs@gmail.com>
2026-04-28 10:39:11 +0200

    Update folder_logic.py DocStrings

--- src/gui_core/folder_logic.py
+++ src/gui_core/folder_logic.py
@@ -2,10 +2,42 @@ import os
 from pathlib import Path
 
 def check_for_single_image_file_in_folder(current_path, file_ending):
+    """Checks for number of files in each subfolder within current path
+    
+    Args:
+    ========
+    current_path: str
+        path-like object telling where to search for files
+    file_ending: str
+        file extension to search for file type (e.g., tiff, tif, nd2)
+        
+    Returns:
+    ========
+    len(files): int
+        Number of files within a current_path
+    """
     files = [file for file in os.listdir(current_path) if file.endswith(file_ending)]
     return len(files)
 
 def find_valid_folders(main_folder, ext):
+    """Function to find folders with valid image files. 
+       All folders are added to a list for processing with suite2p
+       
+       Args:
+       ========
+        main_folder: str
+            path-like object for experiment folder containing all image files for processing
+        ext: str
+            string-like file extension (e.g., tiff, tif, nd2, mp4 bool
+            Boolean check to check for multiple videos in the same subfolder
+        
+        Returns:
+        ========
+        valid: list
+            list of folders containing image files (one file or multiple 
+            optional: returns experiment folders with all images from one experimental treatment
+                experiment folders can be processed in run_suite2p.py by export_image_files_to_suite2p_format
+       """
     main_folder = Path(main_folder)
 
     if not main_folder.exists():
