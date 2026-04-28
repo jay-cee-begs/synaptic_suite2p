edaa80251aef1df8f436cccbbb4fe83aed502826
JC_home <jcbegs@gmail.com>
2026-04-28 17:05:34 +0200

    Create multvid_registration class model

---
+++ src/gui_core/multivid_reg_model.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class MultiVid_Reg_Params:
+    Treatment_No: 2
+    equal_baseline_treatments: bool = True
+    unequal_treatment_lengths: list = field(default_factory=list)
+    treatment_length_units: str = "seconds"
+    calculate_treatment_stats: bool = True
+    calculate_total_vid_stats: bool = True
+
+
+    def to_dict(self):
+        return self.__dict__
+
+    @staticmethod
+    def from_dict(data: dict):
+        return MultiVid_Reg_Params(**data)
