a0c601ed9cec2251e6abe5af502bb03dcf55c959
JC_home <jcbegs@gmail.com>
2026-04-29 14:00:41 +0200

    Update analysis_model.py variable order

--- src/gui_core/analysis_model.py
+++ src/gui_core/analysis_model.py
@@ -3,14 +3,16 @@ from dataclasses import dataclass
 @dataclass
 class AnalysisParams:
     overwrite_suite2p: bool = False
+    multivid_processing: bool = False
+    use_suite2p_ROI_classifier: bool = False
+    update_suite2p_iscell: bool = True
+    Img_Overlay: str = "max_proj"
+    return_decay_times: bool = True
     skew_threshold: float = 1.0
     compactness_threshold: float = 1.4
     peak_detection_threshold: float = 4.5
     peak_count_threshold: int = 1
     Img_Overlay: str = "max_proj"
-    use_suite2p_ROI_classifier: bool = False
-    update_suite2p_iscell: bool = True
-    return_decay_times: bool = True
 
     def to_dict(self):
         return self.__dict__
