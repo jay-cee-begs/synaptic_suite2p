a18a26ef2d1ddb443df61c9720e61d70a47ba91f
JC_home <jcbegs@gmail.com>
2026-04-30 17:44:37 +0200

    Remove unused multivid_params

--- src/gui_core/multivid_reg_model.py
+++ src/gui_core/multivid_reg_model.py
@@ -7,8 +7,7 @@ class MultiVid_Reg_Params:
     equal_baseline_and_treatments: bool = True
     unequal_treatment_lengths: list = field(default_factory=list)
     treatment_length_units: str = "frames"
-    calculate_treatment_stats: bool = True
-    calculate_total_vid_stats: bool = True
+
 
 
     def to_dict(self):
